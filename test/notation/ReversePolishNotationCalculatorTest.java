fc7407c31cba57a1228ca2116df1f57cddf5c1c1
gray-alt <gray-alt@yandex.ru>
2023-03-13 15:41:22 +0300

    test: добавить тесты для проверки обратной польской нотации

--- test/notation/ReversePolishNotationCalculatorTest.java
+++ test/notation/ReversePolishNotationCalculatorTest.java
@@ -1,11 +1,34 @@
 package notation;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class ReversePolishNotationCalculatorTest {
+    private static ReversePolishNotationCalculator calculator;
+
+    @BeforeAll
+    public static void beforeAllTests() {
+        calculator = new ReversePolishNotationCalculator();
+    }
 
     @Test
     public void shouldCalculateAddition() {
+        Assertions.assertEquals(10, calculator.calculatePolishNotation("5 5 +"));
+    }
 
+    @Test
+    public void shouldCalculateSubtraction() {
+        Assertions.assertEquals(3, calculator.calculatePolishNotation("5 2 -"));
+    }
+
+    @Test
+    public void shouldCalculateMultiplication() {
+        Assertions.assertEquals(-10, calculator.calculatePolishNotation("5 -2 *"));
+    }
+
+    @Test
+    public void shouldCalculateWithMultiSpace() {
+        Assertions.assertEquals(10, calculator.calculatePolishNotation("5     2 *"));
     }
 }
